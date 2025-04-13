69fc4eb03c778a5a90fd35953bf3b72e66ef5435
impact66 <youkiborec@gmail.com>
2025-04-13 10:20:11 +0200

    text
    text

--- index.php
+++ index.php
@@ -2,57 +2,57 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-    if (!isset($_GET["category"])) {
-
-    $dirs = glob('produkty/*');
-
-    foreach ($dirs as $dir) {
-
-    $category_name = basename($dir);
-    $category = str_replace("-", " ", $category_name);
-    $category = ucwords($category);
-
-    echo "<a href='?category=$category_name'>$category</a>";
-    }
-    } 
-    else {
-
-    $dirs = glob('produkty/*');
-
-    foreach ($dirs as $dir) {
-
-    $category_name = basename($dir);
-    $category = str_replace("-", " ", $category_name);
-    $category = ucwords($category);
-
-    echo "<a href='?category=$category_name'>$category</a>";
-    }
-
-
-    $category_name = $_GET["category"];
-    $category = str_replace("-", " ", $category_name);
-    $category = ucwords($category);
-
-    echo "<h2>$category</h2>";
-    $images = glob("produkty/$category_name/thumbs/*");
-
-    foreach ($images as $image) {
-
-    $image_name = basename($image);
-    $image_url = "produkty/$category_name/images/$image_name";
-
-
-    echo "<a href='$image_url'><img src='$image'></a>";
-    }
-    }
-?>
     
+
+
+<?php
+$maturita = [
+    [
+        'color'=> '#696969',
+        'text' => 'Prvá veta je takáto',
+        'url' => 'https://www.zilinak.sk/',
+        'font-weight' => '200',
+        'font-size' => '20px',
+    ],
+    [
+        'color'=> '#6b8e23',
+        'text' => 'Druhá veta je takáto',
+        'url' => 'https://www.youtube.com/',
+        'font-weight' => '600',
+        'font-size' => '30px',
+    ],
+    [
+        'color'=> '#afeeee',
+        'text' => 'Tretia veta je takáto',
+        'url' => 'https://soaza.edupage.org/',
+        'font-weight' => '800',
+        'font-size' => '16px',
+    ],
+    [
+        'color'=> '#ff4500',
+        'text' => 'Štvrtá veta je takáto',
+        'url' => 'https://www.learn2code.sk/prihlasenie',
+        'font-weight' => '200',
+        'font-size' => '25px',
+    ],
+    [
+        'color'=> '#ff4538',
+        'text' => 'Piata veta je takáto',
+        'url' => 'https://www.sme.sk/',
+        'font-weight' => '800',
+        'font-size' => '18px',
+    ],
+];
+
+foreach ($maturita as $item) {
+    echo "<p style='color: {$item['color']}; font-weight: {$item['font-weight']}; font-size: {$item['font-size']};'>
+            <a href='{$item['url']}' target='_blank' style='color: inherit;'>{$item['text']}</a>
+          </p>";
+}
+?>
 </body>
 </html>
